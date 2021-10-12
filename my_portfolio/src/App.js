1eca4a6dbb0b4aebd8635844fc287181f2f22f7c
rigo0523 <limon1285@gmail.com>
2021-10-11 23:38:05 -0500

    set up nav bar and redux features

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+
+      <Switch>
+        <Route path="/">{/* Home component */}</Route>
+        <Route path="/about">{/* about component */}</Route>
+        <Route path="/projects">{/* projects component */}</Route>
+        <Route path="/contact">{/* contact component */}</Route>
+      </Switch>
     </div>
   );
 }
